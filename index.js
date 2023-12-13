9df950df80572da17f1343b74ff1fac2a92eda3f
Team A <alishatri21@gmail.com>
2023-12-13 09:34:41 +0100

    update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = 3000;
 
+app.use(cors());
+app.use(bodyParser.json());
+    
 
 app.listen(port, () => {
   console.log(port, `You are in port ${port}`);
